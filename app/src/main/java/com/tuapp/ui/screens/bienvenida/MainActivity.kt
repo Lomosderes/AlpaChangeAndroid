09f541390e4e899677f134f7cb2fb1f363418df2
Lomosderes <176005885+Lomosderes@users.noreply.github.com>
2026-04-29 03:07:36 -0500

    v1.0

---
+++ app/src/main/java/com/tuapp/ui/screens/bienvenida/MainActivity.kt
@@ -0,0 +1,14 @@
+// Pantalla de bienvenida (splash) de AlpaChange
+package com.tuapp.ui.screens.bienvenida
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tuapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
